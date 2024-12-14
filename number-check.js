82e8f9b3743d8a385d66d7fd2f7510425f124ca6
venkatesh-narala <venkateshnarala66@gmail.com>
2024-12-14 11:11:13 +0530

    Add number check program.

---
+++ number-check.js
@@ -0,0 +1,14 @@
+x=prompt("Enter the number")
+
+if (x>0){
+    document.write(x, " is a positive number")
+}
+else if (x<0){
+    document.write(x," is a negitive number")
+}
+else if (x==0){
+    document.write("it is a zero value")
+}
+else {
+    document.write("you not enter number")
+}
