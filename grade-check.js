c3af81fa547ca6c7a2833451f4ae095e8b824cdf
venkatesh-narala <venkateshnarala66@gmail.com>
2024-12-14 15:19:13 +0530

    add grade checking marks.

---
+++ grade-check.js
@@ -0,0 +1,18 @@
+var marks=prompt("Enter your marks")
+
+if(marks>=90){
+    document.write("you got a A grade")
+}
+else if (marks>=80){
+    document.write("you got a B grade")
+}
+else if (marks>=70){
+    document.write("you got a C grade")
+}
+else if (marks>=60){
+    document.write("you got a D grade")
+}
+else if (marks<=59){
+    document.write("you got a F grade")
+}
+
