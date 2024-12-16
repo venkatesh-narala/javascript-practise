c6e9ea353da6b5b43316e89cdd50f5c91bebb14c
venkatesh-narala <venkateshnarala66@gmail.com>
2024-12-16 14:36:21 +0530

    add greater numvers checking

---
+++ greater-numbers.js
@@ -0,0 +1,15 @@
+x=prompt("Enter the first number:")
+y=prompt("Enter the second number:")
+
+if (x>y){
+   document.write("The greater number is",x)
+}
+else if (y>x){
+   document.write("The greater number is",y)
+}
+else if (x==y){
+   document.write("both values are equal")
+}
+else {
+   document.write("you not enter the numbers")
+}
